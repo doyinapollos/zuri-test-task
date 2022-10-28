3f419fb0cee25d2c4a55fdcbc27a44dc93940c46
ebiapollos <doyinapollos@gmail.com>
2022-10-28 17:40:19 +0100

    web design

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import img1 from "./photo/image-1.jpg";
 
 function App() {
   return (
-    <div className="">
-      <div id="profile__img" className="">
-        <img className="w-24 h-24" src={img1} alt="" />
+    <div className="app">
+      <div className="main">
+        <div id="profile__img" className="">
+          <img className="w-24 h-24 rounded-full" src={img1} alt="" />
+        </div>
+
+        <div id="twitter" className="links-content">
+          @ebiapollos
+        </div>
+
+        <div className="links-content">Zuri team</div>
+
+        <div className="links-content">Python books</div>
+
+        <div className="links-content">Background check for coders</div>
+
+        <div className="links-content">Design books</div>
       </div>
 
-      <div id="twitter" className="">
-        @ebiapollos
-      </div>
-
-      <div className="">Zuri team</div>
-
-      <div className="">Python books</div>
-
-      <div className="">Background check for coders</div>
-
-      <div className="">Design books</div>
-
-      <footer className="">
-        <div className="">
+      <footer className="footer">
+        <div className="footer-sub-div">
           <span>Zuri.Internship</span>
 
           <span>HGN Internship 9 Frontend Task</span>
 
-          <div className="">
-            <img src="" alt="" />
+          <div className="flex">
+            <img
+              className="w-8 h-8"
+              src="https://www.mappr.co/wp-content/uploads/2021/11/africa-continent-blank-black-white-edited.jpg"
+              alt=""
+            />
 
             <span>
               INGRESIVE
