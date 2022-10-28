320fd8a8ba63b246f35672512629a207dae5cf2e
ebiapollos <doyinapollos@gmail.com>
2022-10-28 14:53:23 +0100

    frame work 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,24 @@ function App() {
 
       <div className="">Background check for coders</div>
 
-      <div className=""></div>
+      <div className="">Design books</div>
+
+      <footer className="">
+        <div className="">
+          <span>Zuri.Internship</span>
+
+          <span>HGN Internship 9 Frontend Task</span>
+
+          <div className="">
+            <img src="" alt="" />
+
+            <span>
+              INGRESIVE
+              <br /> FOR GOOD
+            </span>
+          </div>
+        </div>
+      </footer>
 
       <h1 className="text-2xl font-bold underline text-blue-600">
         Hello world!
