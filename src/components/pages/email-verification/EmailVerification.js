118108a8c5c4d8bb3b472e558c4cb8187f6b7e80
ebiapollos <doyinapollos@gmail.com>
2022-11-01 21:50:25 +0100

    commit one

--- src/components/pages/email-verification/EmailVerification.js
+++ src/components/pages/email-verification/EmailVerification.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function EmailVerification() {
-  return <div></div>;
+  return (
+    <div className=" flex flex-col justify-center items-center w-full h-auto bg-gray-400">
+      <div className="w-1/4 h-auto bg-white">
+        <div className="w-80 h-96 mt-24 my-10 bg-sky-300">
+          <div className="w-44 h-11">Hi Ebuka</div>
+        </div>
+      </div>
+    </div>
+  );
 }
